95890383b9068a1a549be3f239854db6e287f84f
S-Mlangeni <smlangeni21@gmail.com>
2021-06-29 11:17:43 +0200

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/form_client"));
 /* ...static(current root folder + the dist folder that contains
 our statics files that will be built when "ng build" is run)*/
 
@@ -10,5 +10,5 @@ app.listen(process.env.PORT || 8000);
 
 //To allow angular to handling routing instead of this server file:
 app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname + "/dist/index.html"));
+    res.sendFile(path.join(__dirname + "/dist/form_client/index.html"));
 })
